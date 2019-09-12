1bb46782fd86f94fe445331be09f7d77ead8df41
georgealexnadruiancu <alex1gtp@gmail.com>
2019-09-12 15:19:29 +0300

    init project

---
+++ src/components/create-todo.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateTodos extends Component {
+    render() {
+        return (
+            <div>
+                <p>welcome to create todos</p>
+            </div>
+        )
+    }
+}
---
+++ src/components/todos-list.component.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class TodosList extends Component{
+    render(){
+        return(
+            <div>
+                <p>welcome to list of components</p>
+            </div>
+        )
+    }
+}
